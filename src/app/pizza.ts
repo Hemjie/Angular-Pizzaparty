0dc2242a2a5c2eeda974db1016409e6c8ce3fc76
Hemjie <marjolaine.vilain@gmail.com>
2020-12-14 13:43:29 +0100

    création d'une classe

---
+++ src/app/pizza.ts
@@ -0,0 +1,10 @@
+/**
+ * Cette classe me permet de structurer ce que contient une pizza.
+ * L'export permet d'importer la classe partout dans mon application
+ */
+
+export class Pizza {
+    id: number;
+    name: string;
+    price: number;
+}
