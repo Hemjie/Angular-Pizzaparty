02b866aa452b1fd6567cad70faafb5c070202ca6
Hemjie <marjolaine.vilain@gmail.com>
2020-12-14 16:46:30 +0100

    déplacement des éléments concernés dans le composant pizza-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Pizza } from './pizza';
 
 @Component({
   selector: 'app-root',
@@ -8,25 +7,6 @@ import { Pizza } from './pizza';
 })
 export class AppComponent {
   title = 'Pizza Party';
-  name = '4 fromages';
-  // maSuperPizza est "typée" avec la classe Pizza
-  maSuperPizza: Pizza;
-
-  // On peut typer un tableau
-  mesPizzas: Pizza[] = [ //cette variable est un tableau d'objets Pizza
-    {id: 1, name: 'Reine', price: 12, image: 'reine.jpg'},
-    {id: 2, name: '4 fromages', price: 13, image: '4-fromages.jpg'},
-    {id: 3, name: 'Orientale', price: 11, image: 'orientale.jpg'},
-    {id: 4, name: 'Cannibale', price: 9, image: 'cannibale.jpg'}
-  ];
-
-  //on peut créer une fonction / méthode
-  onSelect(pizza: Pizza) {  //possibilité de typer une fonction
-    console.log(pizza);
-    //on va modifier la valeur de maSuperPizza
-    console.log(this); //this est pour AppComponent
-    this.maSuperPizza = pizza;
-  }
-
+  
 }
 
--- src/app/pizza-list/pizza-list.component.ts
+++ src/app/pizza-list/pizza-list.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Pizza } from '../pizza';
+
 @Component({
   selector: 'app-pizza-list',
   templateUrl: './pizza-list.component.html',
   styleUrls: ['./pizza-list.component.scss']
 })
 export class PizzaListComponent implements OnInit {
+  // maSuperPizza est "typée" avec la classe Pizza
+  maSuperPizza: Pizza;
+
+  // On peut typer un tableau
+  mesPizzas: Pizza[] = [ //cette variable est un tableau d'objets Pizza
+    {id: 1, name: 'Reine', price: 12, image: 'reine.jpg'},
+    {id: 2, name: '4 fromages', price: 13, image: '4-fromages.jpg'},
+    {id: 3, name: 'Orientale', price: 11, image: 'orientale.jpg'},
+    {id: 4, name: 'Cannibale', price: 9, image: 'cannibale.jpg'}
+  ];
+
+  //on peut créer une fonction / méthode
+  onSelect(pizza: Pizza) {  //possibilité de typer une fonction
+    console.log(pizza);
+    //on va modifier la valeur de maSuperPizza
+    console.log(this); //this est pour AppComponent
+    this.maSuperPizza = pizza;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  
 }
