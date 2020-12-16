3467ff2735c65620756da6e2a4632328a6693045
Hemjie <marjolaine.vilain@gmail.com>
2020-12-16 13:47:03 +0100

    création du formulaire

--- src/app/pizza-add/pizza-add.component.ts
+++ src/app/pizza-add/pizza-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pizza } from '../pizza';
 
 @Component({
   selector: 'app-pizza-add',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pizza-add.component.scss']
 })
 export class PizzaAddComponent implements OnInit {
+  //on prépare une instance vide d'une pizza
+  pizza: Pizza = new Pizza();
+  //propriété (chp, var) : type = Objet();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //création de la méthode qui s'exécute à l'écoute du ngSubmit du formulaire
+  addPizza() { 
+    console.log(this.pizza);
   }
 
 }
